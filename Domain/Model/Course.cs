1b8cd701f676c74b1cc6e8f4d28bb275509a72b0
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2025-09-27 14:58:08 +0200

    fix(domain): updated namespace for entities

--- Domain/Model/Course.cs
+++ Domain/Model/Course.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StudentManagement.Model
+namespace StudentManagement.Domain.Model
 {
     internal class Course
     {
