ab0a40858c1c208330c1d4bef03d762ce173af5a
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2025-09-28 16:06:14 +0200

    feat(migrations): updated Student entity, and added MiddleName migrations and script

---
+++ Changed-Based/Artifacts/V2__AddMiddleName.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Student] ADD [MiddleName] nvarchar(max) NOT NULL DEFAULT N'';
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250928134101_V2__AddMiddleName', N'9.0.9');
+
+COMMIT;
+GO
+
---
+++ Migrations/20250928134101_V2__AddMiddleName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class V2__AddMiddleName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MiddleName",
+                table: "Student",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MiddleName",
+                table: "Student");
+        }
+    }
+}
