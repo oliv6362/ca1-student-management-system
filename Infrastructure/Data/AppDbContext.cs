fd98cbd6a6a658dd2263027a43d8502bb934f91f
oliv6362 <33200860+oliv6362@users.noreply.github.com>
2025-10-01 14:22:55 +0200

    feat(state-based): Updated AppDbContext, and added deployment InitialSchema and desired state schema script.

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -12,6 +12,37 @@ internal class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        //STUDENT
+        modelBuilder.Entity<Student>(e =>
+        {
+            e.ToTable("Student");
+            e.HasKey(s => s.Id);
 
+        });
+
+        //COURSE
+        modelBuilder.Entity<Course>(e =>
+        {
+            e.ToTable("Course");
+            e.HasKey(c => c.Id);
+
+        });
+
+        //ENROLLMENT
+        modelBuilder.Entity<Enrollment>(e =>
+        {
+            e.ToTable("Enrollment");
+            e.HasKey(en => en.Id);
+
+            //FK - Student
+            e.HasOne(en => en.Student)
+                .WithMany(s => s.Enrollments)
+                .HasForeignKey(en => en.StudentId);
+
+            //FK - Course
+            e.HasOne(en => en.Course)
+                .WithMany(c => c.Enrollments)
+                .HasForeignKey(en => en.CourseId);
+        });
     }
 }
---
+++ State-Based/State/schema.sql
@@ -0,0 +1,41 @@
+﻿-- Desired state script for V1
+CREATE TABLE [Course] (
+    [Id] int NOT NULL IDENTITY,
+    [Title] nvarchar(max) NOT NULL,
+    [Credits] int NOT NULL,
+    CONSTRAINT [PK_Course] PRIMARY KEY ([Id])
+);
+GO
+
+
+CREATE TABLE [Student] (
+    [Id] int NOT NULL IDENTITY,
+    [FirstName] nvarchar(max) NOT NULL,
+    [LastName] nvarchar(max) NOT NULL,
+    [Email] nvarchar(max) NOT NULL,
+    [EnrollmentDate] datetime2 NOT NULL,
+    CONSTRAINT [PK_Student] PRIMARY KEY ([Id])
+);
+GO
+
+
+CREATE TABLE [Enrollment] (
+    [Id] int NOT NULL IDENTITY,
+    [StudentId] int NOT NULL,
+    [CourseId] int NOT NULL,
+    [Grade] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Enrollment] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Enrollment_Course_CourseId] FOREIGN KEY ([CourseId]) REFERENCES [Course] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Enrollment_Student_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [Student] ([Id]) ON DELETE CASCADE
+);
+GO
+
+
+CREATE INDEX [IX_Enrollment_CourseId] ON [Enrollment] ([CourseId]);
+GO
+
+
+CREATE INDEX [IX_Enrollment_StudentId] ON [Enrollment] ([StudentId]);
+GO
+
+
